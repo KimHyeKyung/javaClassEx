cff70a941b8439abafea50869bfaa2fecc9fd402
hyekyung <kimhye_1218@naver.com>
2022-08-04 10:38:18 +0900

    조건문

--- src/com/javaex/helloworld/ScannerIf.java
+++ src/com/javaex/helloworld/ScannerIf.java
@@ -20,14 +20,27 @@ public class ScannerIf {
 		System.out.print("점수를 입력하세요: ");
 		Scanner sc = new Scanner(System.in);
 		int num = sc.nextInt();
-
+		//경우1
+//		if (num >= 90) {
+//			System.out.println("A등급");
+//		}else if((num >= 80) && (num <= 89)) {
+//			System.out.println("B등급");
+//		}else if((num >= 70) && (num <= 79)) {
+//			System.out.println("C등급");
+//		}else if((num >= 60) && (num <= 69)) {
+//			System.out.println("D등급");
+//		}else {
+//			System.out.println("F등급");
+//		}
+		
+		//경우2 -> 이게 더 간단!!
 		if (num >= 90) {
 			System.out.println("A등급");
-		}else if((num >= 80) && (num <= 89)) {
+		}else if(num >= 80) {
 			System.out.println("B등급");
-		}else if((num >= 70) && (num <= 79)) {
+		}else if(num >= 70) {
 			System.out.println("C등급");
-		}else if((num >= 60) && (num <= 69)) {
+		}else if(num >= 60) {
 			System.out.println("D등급");
 		}else {
 			System.out.println("F등급");
