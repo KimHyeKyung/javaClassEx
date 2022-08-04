308cd85290db83c2ab8991867b85f835fa1e67d9
hyekyung <kimhye_1218@naver.com>
2022-08-04 12:28:39 +0900

    반복문(구구단, 별찍기)

---
+++ src/com/javaex/helloworld/ForEx04.java
@@ -0,0 +1,24 @@
+package com.javaex.helloworld;
+
+public class ForEx04 {
+	public static void main(String[] args) {
+		//구구단 출력
+//		for(int i = 2; i < 10; i++) {
+//			for(int j = 1; j < 10; j++) {
+//				System.out.println(i + "*" + j + "= " + i*j);
+//			}
+//		}
+		
+		
+		//별찍기
+		for(int i = 1; i <= 5; i++) {
+			for(int j = 0; j <= i-1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		
+		
+	}
+}
