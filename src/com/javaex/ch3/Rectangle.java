81b2c16fc6837988ad07037c2dace5df3c1adc47
hyekyung <kimhye_1218@naver.com>
2022-08-11 10:37:11 +0900

    자바 기본API

---
+++ src/com/javaex/ch3/Rectangle.java
@@ -0,0 +1,59 @@
+package com.javaex.ch3;
+
+public class Rectangle {
+  private int width;
+  private int height;
+  public int getWidth() {
+    return width;
+  }
+  public void setWidth(int width) {
+    this.width = width;
+  }
+  public int getHeight() {
+    return height;
+  }
+  public void setHeight(int height) {
+    this.height = height;
+  }
+  /**
+   * 
+   */
+  public Rectangle() {
+    super();
+  }
+  /**
+   * @param width
+   * @param height
+   */
+  public Rectangle(int width, int height) {
+    super();
+    this.width = width;
+    this.height = height;
+  }
+  @Override
+  public String toString() {
+    return "Rectangle [width=" + width + ", height=" + height + "]";
+  }
+  
+  public int getArea() {
+    return this.height * this.width;
+  }
+  
+  public boolean equals(Object obj) {
+	  
+    Rectangle r = ((Rectangle) obj);
+    
+    if(this.hashCode() == r.hashCode()) {
+      return true;
+    }else if(this.width == r.width && this.height == r.height) {
+      return true;
+    }else if(this.getArea() == r.getArea() ) {
+      return true;
+    }else {
+      return false;
+    }
+  }
+  
+
+
+}
